e461c28632c49674ab0cc1ab311320123e4bb080
Jeremiah Deleon <jeremiah.deleon@advantagesolutions.net>
2018-12-05 16:52:48 -0800

    Added templates for Team pages. Added routs for Team Login page

--- basketball_league.py
+++ basketball_league.py
@@ -1,10 +1,9 @@
 from __future__ import print_function
-from flask import Flask, render_template, url_for, flash, redirect
+from flask import Flask, render_template, url_for, flash, redirect, session
 from flask_pymongo import PyMongo
 from team_registration import TeamRegistration
 from team_login import TeamLogin
 from team import Team
-import sys
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = 'R0HU997LNU9HKE48'
@@ -30,7 +29,6 @@ standings = [
 def home():
     return render_template('home.html', standings=standings, show_home=1)
 
-
 @app.route("/team_registration", methods=["GET", "POST"])
 def team_registration():
     form = TeamRegistration()
@@ -41,28 +39,46 @@ def team_registration():
         for player in form.players.data:
             team.add_player(player['first_name'], player['last_name'])
             team.assign_player_to_team(player['first_name'], player['last_name'], form.team_name.data)
-        flash("Account created for %s" % form.gm_email.data, 'success')
+        session['gm_email'] = form.gm_email.data
+        flash("Account created for %s" % session['gm_email'], 'success')
         return redirect(url_for('team_registration_confirmation'))
     else:
         # return form.errors
         return render_template('team_registration.html', show_home=0, title='Team Registration', form=form)
 
+@app.route("/teams")
+def teams():
+    team = Team()
+    teams = team.get_teams()
 
-@app.route("/team_login")
+    return render_template('teams.html', title='Teams', team_list=teams)
+
+@app.route("/teams/<team_id>")
+def team_page(team_id):
+    team = Team()
+    team_data = team.get_team_data_by_id(team_id)
+
+    return render_template('team_information.html', title='Teams', team_info_list=team_data)
+
+@app.route("/team_login", methods=['POST'])
 def team_login():
     form = TeamLogin()
-    return render_template('team_login.html', show_home=0, title='Team Login', form=form)
 
+    # if 'gm_email' in session:
+    #     return 'you are logged in as ' + session['gm_email']
+    return render_template('team_login.html', show_home=0, title='Team Login', form=form)
 
 @app.route("/team_registration_confirmation")
 def team_registration_confirmation():
     return render_template('team_registration_confirmation.html')
 
-
 @app.route("/admin")
 def admin():
     return "this is the admin page"
 
+@app.route("/admin/team/<team_id>")
+def admin_team(team_id):
+    return "this is the admin team page"
 
 @app.route('/add_players')
 def add_players():
@@ -70,15 +86,3 @@ def add_players():
     team.add_user('jdeleon@gmail.com', 'password')
     return 'Added!'
 
-
-@app.route('/add_team')
-def add_team():
-    roster = Rosters()
-    roster.add_team("Clippers", "Jeremiah De Leon")
-    return 'Team added!'
-
-
-@app.route('/update_team')
-def update_team():
-    roster = Rosters()
-    roster.update_team("Clippers", "Michael", "Jordan")
--- team.py
+++ team.py
@@ -4,14 +4,15 @@ import bcrypt
 
 app = Flask(__name__)
 
-app.config['MONGO_URI'] = 'mongodb://jdeleon:gamebang08@ds123434.mlab.com:23434/jdleague'
-# app.config['MONGO_URI'] = 'mongodb://localhost:27017/jdleague'
+# app.config['MONGO_URI'] = 'mongodb://jdeleon:gamebang08@ds123434.mlab.com:23434/jdleague'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/jdleague'
 
 mongo = PyMongo(app)
 
 
 class Team(object):
 
+    ##### MongoDB Interactions #####
     def add_user(self, email, password):
         users = mongo.db.users
         existing_user = users.find_one({'email': email})
@@ -26,12 +27,21 @@ class Team(object):
                 }
             )
 
+    def gm_login(self, gm_email, pw):
+        users = mongo.db.users
+        login = users.find_one({"email": gm_email})
+
+        if login:
+            if bcrypt.hashpw(pw.encode('utf-8'), bcrypt.gensalt() == login['password'].encode('utf-8')):
+                return login
+
     def add_team(self, team_name, general_manager):
         teams = mongo.db.teams
+        team_id = ''.join(team_name.split()).lower()
         teams.insert(
             {
-                "team_id": 1,
                 "team_name": team_name,
+                "team_id": team_id,
                 "general_manager": general_manager,
                 "players": [
 
@@ -48,25 +58,53 @@ class Team(object):
             }
         )
 
+    def get_player(self, first_name, last_name):
+        players = mongo.db.players
+        player = players.find_one({"first_name": first_name, "last_name": last_name})
+
+        return player
+
     def assign_player_to_team(self, first_name, last_name, team_name):
         teams = mongo.db.teams
+        players = mongo.db.players
+        player = players.find_one({"first_name": first_name, "last_name": last_name})
 
-        teams.update(
-            {"team_name": team_name},
-            {"$push":
-                 {
-                     "players": {
-                         "first_name": first_name,
-                         "last_name": last_name
-                     }
-                 }
-            }
-        )
+        teams.update_one({"team_name": team_name},
+                         {"$push": {"players": player}}
+                         )
 
 
+    def get_teams(self):
+        """
+        Returns a two dimensional list of team names.
+        :return:
+        """
+        team = mongo.db.teams
+        cursor = team.find({})
+        team_list = [[doc['team_name'],''.join(doc['team_name'].split()).lower()] for doc in cursor]
+
+        # for doc in cursor:
+        #     team_list.append(doc)
+
+        return team_list
+
+    def get_team_data_by_id(self, team_id):
+        team = mongo.db.teams
+        cursor = team.find({"team_id": team_id})
+        team_data = []
+        for doc in cursor:
+            team_data.append(doc)
+        return team_data
 
 if __name__ == '__main__':
     from team import Team
+    import json
 
     team = Team()
-    team.assign_player_to_team('rob', 'de leon', 'black panther')
+    # teams = team.get_team_data("Hawks")
+    # print teams
+    login = team.gm_login('test@email.com', 'test')
+
+
+    print login
+
